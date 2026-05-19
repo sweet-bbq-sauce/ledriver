dec41b62e8b3fc5cd7aed6056ca7bd4700dd6794
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-05-19 15:56:37 +0200

    feat: add `version.json` file

--- main/main.c
+++ main/main.c
@@ -1,10 +1,12 @@
-#include <ledriver_app_config.h>
-#include <ledriver_wifi.h>
-
 #include <dirent.h>
+#include <sys/stat.h>
+
 #include <esp_log.h>
 #include <esp_spiffs.h>
-#include <sys/stat.h>
+
+#include <ledriver_app_config.h>
+#include <ledriver_device_info.h>
+#include <ledriver_wifi.h>
 
 static const char *TAG = "webpanel";
 static const char *SPIFFS_PARTITION_LABEL = "webpanel";
@@ -42,6 +44,9 @@ void app_main(void) {
 
     ESP_ERROR_CHECK(esp_vfs_spiffs_register(&conf));
 
+    ESP_LOGI(TAG, "Hardware version: %s", LEDRIVER_HARDWARE_VERSION);
+    ESP_LOGI(TAG, "Firmware build number: %d", LEDRIVER_FIRMWARE_BUILD_NUMBER);
+
     size_t total = 0;
     size_t used = 0;
     ESP_ERROR_CHECK(esp_spiffs_info(SPIFFS_PARTITION_LABEL, &total, &used));
@@ -59,12 +64,10 @@ void app_main(void) {
             ESP_LOGI(TAG, "  WiFi SSID: %s", config.wifi_ssid);
             ESP_LOGI(TAG, "  Update Base URL: %s", config.update_base_url);
 
-            const ledriver_wifi_config_t wifi_config = {
-                .ssid = config.wifi_ssid,
-                .password = config.wifi_password,
-                .max_retries = 5,
-                .connect_timeout_ms = 10000
-            };
+            const ledriver_wifi_config_t wifi_config = {.ssid = config.wifi_ssid,
+                                                        .password = config.wifi_password,
+                                                        .max_retries = 5,
+                                                        .connect_timeout_ms = 10000};
 
             err = ledriver_wifi_start(&wifi_config);
             if (err != ESP_OK) {

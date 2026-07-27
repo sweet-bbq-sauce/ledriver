3ba74542f7d0a1126e266ddbcbdee3feb613c41a
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-07-27 13:29:59 +0200

    doc: improve LDR protocol specification

--- components/ledriver_udp_server/include/ledriver/udp_server.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <esp_err.h>
-#include <lwip/sockets.h>
-
-esp_err_t ledriver_udp_server_listen(const struct sockaddr_storage *addr);
--- main/main.c
+++ main/main.c
@@ -11,7 +11,6 @@
 #include <ledriver/device_version.h>
 #include <ledriver/led.h>
 #include <ledriver/panel_partition.h>
-#include <ledriver/udp_server.h>
 #include <ledriver/wifi.h>
 
 static const char *TAG = "main";
@@ -111,11 +110,6 @@ void app_main(void) {
         addr_in->sin_family = AF_INET;
         addr_in->sin_port = htons(12345);
         addr_in->sin_addr.s_addr = INADDR_ANY;
-
-        err = ledriver_udp_server_listen(&addr);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to initialize UDP server: %s", esp_err_to_name(err));
-        }
     } else {
         ESP_LOGE(TAG, "Failed to initialize config: %s", esp_err_to_name(err));
         return;
